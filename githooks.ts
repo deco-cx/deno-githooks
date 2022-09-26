f428663f9552336f2986a714714691d4c0d8abce
Guilherme Rodrigues <gui@futuredriven.dev>
2022-09-26 13:13:40 -0300

    Initial commit

---
+++ githooks.ts
@@ -0,0 +1,27 @@
+export async function readJson(filePath: string): Promise<unknown> {
+  try {
+    const jsonString = await Deno.readTextFile(filePath);
+    return JSON.parse(jsonString);
+  } catch (err) {
+    err.message = `${filePath}: ${err.message}`;
+    throw err;
+  }
+}
+
+type DenoConfig = {
+  githooks: Record<string, string[]>;
+};
+
+const denoConfig = await readJson("./deno.json") as DenoConfig;
+console.log(denoConfig);
+
+const hooks = Object.keys(denoConfig.githooks);
+
+for (const hook of hooks) {
+  const task = denoConfig.githooks[hook];
+  const hookPath = `./.git/hooks/${hook}`;
+  const hookScript = `#!/bin/sh
+exec deno task ${task}`;
+  await Deno.writeTextFile(hookPath, hookScript);
+  await Deno.chmod(hookPath, 0o755);
+}
