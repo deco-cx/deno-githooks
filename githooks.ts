63734e94dfd730c6344b56db4d07581e96ec6362
Samuel Kopp <62482066+boywithkeyboard@users.noreply.github.com>
2023-06-18 23:25:23 +0200

    edit githooks.ts

--- githooks.ts
+++ githooks.ts
@@ -1,60 +1,100 @@
-export type Githooks = {
-  githooks: Record<string, string[]>;
-};
+import {
+  brightGreen,
+  brightRed,
+  gray,
+} from "https://deno.land/std@0.192.0/fmt/colors.ts";
+import * as jsonc from "https://deno.land/std@0.192.0/jsonc/parse.ts";
+import * as yaml from "https://deno.land/std@0.192.0/yaml/parse.ts";
 
-export type GithooksOptions = {
-  getConfig: () => Promise<Githooks>;
-  verbose?: boolean;
-};
+/* find file ---------------------------------------------------------------- */
 
-export async function readJson(filePath: string): Promise<unknown> {
+type ConfigFile =
+  | `${string}.json`
+  | `${string}.jsonc`
+  | `${string}.yaml`
+  | `${string}.yml`;
+
+async function findFile(
+  ...paths: ConfigFile[]
+): Promise<
+  | Record<string, string>
+  | {
+    githooks: Record<string, string>;
+    [rest: string]: unknown;
+  }
+  | undefined
+> {
   try {
-    const jsonString = await Deno.readTextFile(filePath);
+    const content = await Deno.readTextFile(paths[0]);
 
-    return JSON.parse(jsonString);
-  } catch (err) {
-    err.message = `${filePath}: ${err.message}`;
-
-    throw err;
-  }
-}
-
-export function readDenoJson(): Promise<Githooks> {
-  return readJson("./deno.json") as Promise<Githooks>;
-}
-
-const defaultOptions: GithooksOptions = {
-  getConfig: readDenoJson,
-  verbose: true,
-};
-
-export async function setupGithooks(opts: GithooksOptions = defaultOptions) {
-  const config = await opts.getConfig();
-
-  if (!config.githooks) {
-    opts.verbose &&
-      console.log("No githooks found in deno.json — skipping setup.");
-  } else {
-    const hooks = Object.keys(config.githooks);
-
-    for (const hook of hooks) {
-      const task = config.githooks[hook];
-      const hookPath = `./.git/hooks/${hook}`;
-      const hookScript = `#!/bin/sh\nexec deno task ${task}`;
-
-      await Deno.writeTextFile(hookPath, hookScript);
-
-      if (Deno.build.os !== "windows") {
-        await Deno.chmod(hookPath, 0o755);
-      }
+    if (paths[0].endsWith(".json")) {
+      return JSON.parse(content);
+    } else if (paths[0].endsWith(".jsonc")) {
+      return jsonc.parse(content) as Record<string, string>;
+    } else {
+      return yaml.parse(content) as Record<string, string>;
     }
+  } catch (_) {
+    paths.shift();
 
-    opts.verbose &&
-      console.log("Githooks setup successfully:", hooks.join(", "));
+    if (paths.length > 0) {
+      return await findFile(...paths);
+    }
   }
 }
 
-// Execute when called directly
+/* setup hooks -------------------------------------------------------------- */
+
+export async function setup({
+  verbose = true,
+  file,
+}: {
+  file?: ConfigFile;
+  verbose?: boolean;
+} = {}) {
+  const content = file ? await findFile(file) : await findFile(
+    "./githooks.json",
+    "./githooks.jsonc",
+    "./githooks.yaml",
+    "./githooks.yml",
+    "./deno.json",
+    "./deno.jsonc",
+  );
+
+  if (!content) {
+    return verbose &&
+      console.error(brightRed("No githooks found!"));
+  }
+
+  const githooks = (content.githooks ? content.githooks : content) as Record<
+    string,
+    string
+  >;
+
+  const hooks = Object.keys(githooks);
+
+  for (const h of hooks) {
+    const task = githooks[h];
+    const hookPath = `./.git/hooks/${h}`;
+    const hookScript = `#!/bin/sh\nexec deno task ${task}`;
+
+    await Deno.writeTextFile(hookPath, hookScript);
+
+    if (Deno.build.os !== "windows") {
+      await Deno.chmod(hookPath, 0o755);
+    }
+  }
+
+  verbose &&
+    console.info(
+      gray(
+        `${brightGreen("Added githooks:")} ${hooks.join(", ")}`,
+      ),
+    );
+}
+
+/* execute cli -------------------------------------------------------------- */
+
 if (import.meta.main) {
-  await setupGithooks();
+  await setup();
 }
